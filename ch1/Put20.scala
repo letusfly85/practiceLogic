740d44d57b746155f4840c8b4cef1f2844685ecc
shunsuke wada <letusfly95@gmail.com>
2014-06-08 15:13:36 +0900

    replace by %20

--- ch1/Put20.scala
+++ ch1/Put20.scala
@@ -1,7 +1,23 @@
 object Put20 {
     def main(args: Array[String]) {
+        println("\"" + put20(args(0)) + "\"")
     }
 
-    def put20(str: String) {
+    def put20(str: String): String = {
+        var strSize = str.size
+
+        var newStr: String = ""
+        str.map(_.toChar).foreach {c =>
+            if (c == ' ' && strSize > 3) {
+                newStr += "%20"
+                strSize -= 3
+            
+            } else {
+                newStr += c
+                strSize -= 1
+            }
+        }
+
+        newStr
     }
 }
