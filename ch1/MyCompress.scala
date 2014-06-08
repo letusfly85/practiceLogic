78d5a4c14ed51673b6dcd91f0e532856b23bbf01
shunsuke wada <letusfly95@gmail.com>
2014-06-08 20:22:59 +0900

    modify

--- ch1/MyCompress.scala
+++ ch1/MyCompress.scala
@@ -1,8 +1,39 @@
 object MyCompress {
+    var counter: Int  = 0
+
     def main(args: Array[String]) {
+        println(compress(args(0)))
     }
 
     def compress(str: String): String = {
-        str
+        var preChar: Char = str.map(_.toChar).head
+        var newStr:  String = ""
+
+        str.map(_.toChar).foreach {c =>
+            if (c == preChar) {
+                counter += 1
+
+            } else {
+                if (counter > 1) {
+                    newStr += preChar.toString + counter.toString
+
+                } else {
+                    newStr += preChar.toString
+                }
+
+                counter = 1
+            }
+
+            preChar = c
+        }
+
+        if (counter > 1) {
+            newStr += preChar.toString + counter.toString
+
+        } else {
+            newStr += preChar.toString
+        }
+
+        newStr
     }
 }
